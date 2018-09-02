4b7ce1f72aada7e2a4745dce32d47e0aec1f3358
Anuj Jain <jaindrive@hotmail.com>
2018-09-02 13:57:59 +0530

    Add HTML report code

--- OrangeHRM/OrangeHrmTestCase/Login.py
+++ OrangeHRM/OrangeHrmTestCase/Login.py
@@ -1,14 +1,28 @@
 from OrangeHrmPages.PageLogin import PageLogin
 import unittest
 from selenium import webdriver
+import HtmlTestRunner
 
 
 class Test_TCLogin(unittest.TestCase):
+    
     def test_A(self):
         driver = webdriver.Chrome(r"D:\Python\VS\PyPOM\POM\Drivers\chromedriver.exe")
         driver.get('https://opensource-demo.orangehrmlive.com/')
         ObjPageLogin = PageLogin(driver)
         ObjPageLogin.Login("Admin","admin123")
+        driver.close();
+        driver.quit();
+
+    def test_B(self):
+        driver = webdriver.Chrome(r"D:\Python\VS\PyPOM\POM\Drivers\chromedriver.exe")
+        driver.get('https://opensource-demo.orangehrmlive.com/')
+        ObjPageLogin = PageLogin(driver)
+        ObjPageLogin.Login("Admin","admin123")
+        driver.close();
+        driver.quit();
+
+
 
 if __name__ == '__main__':
-    unittest.main(exit=False)
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='D:\\'),exit=False)
