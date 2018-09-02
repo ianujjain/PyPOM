528858356898c65693c4884001be40b23599ad1f
Anuj Jain <jaindrive@hotmail.com>
2018-09-02 09:12:57 +0530

    add New Method
    add New Method

--- POMAutomation/Helper/HelperDateTime.py
+++ POMAutomation/Helper/HelperDateTime.py
@@ -1,13 +1,38 @@
 import datetime
-class HelperDateTime():
-    def GetCurrentTime():
-        CurrentTime = datetime.datetime.time(datetime.datetime.now());
-        return CurrentTime;
+import time
+from dateutil import relativedelta
 
-    def GetTimeDifferenceInMin(PastTime):
-        CurrentTime = datetime.datetime.time(datetime.datetime.now());
-        Min = CurrentTime-PastTime
-        return int((Min.seconds // 60) % 60)
+class HelperDateTime(object):
 
-    def GetTimeDifferenceInSec():
-        pass;
+    @staticmethod
+    def GetPresentTime():
+        CurrentTime = datetime.datetime.now()
+        return CurrentTime
+
+    @staticmethod
+    def GetTimeDifferenceInMinute(PastTime):
+        CurrentTime = HelperDateTime.GetPresentTime()
+        TimeDifference = relativedelta.relativedelta(CurrentTime,PastTime)
+        return TimeDifference.minutes
+
+    @staticmethod
+    def GetTimeDifferenceInSecond(PastTime):
+        CurrentTime = HelperDateTime.GetPresentTime()
+        TimeDifference = relativedelta.relativedelta(CurrentTime,PastTime)
+        return TimeDifference.seconds
+
+    @staticmethod
+    def GetTimeStamp(FormatTimeStamp='%Y_%m_%d_%H_%M_%S'):
+        return datetime.datetime.fromtimestamp(time.time()).strftime(FormatTimeStamp)
+
+    @staticmethod
+    def GetCurrentTime(FormatTime='%H:%M:%S'):
+        return datetime.datetime.fromtimestamp(time.time()).strftime(FormatTime)
+
+    @staticmethod
+    def GetCurrentDate(FormatTimeStamp='%Y-%m-%d'):
+        return datetime.datetime.fromtimestamp(time.time()).strftime(FormatTimeStamp)
+
+    @staticmethod
+    def GetCurrentDateTime(FormatTimeStamp='%Y-%m-%d %H:%M:%S'):
+        return datetime.datetime.fromtimestamp(time.time()).strftime(FormatTimeStamp)
