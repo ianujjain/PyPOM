6e6b2216f2c66d990fd0221541af2b8179e66312
Anuj Jain <jaindrive@hotmail.com>
2018-09-05 21:53:39 +0530

    jenkins testing

--- OrangeHRM/OrangeHrmTestCase/Login.py
+++ OrangeHRM/OrangeHrmTestCase/Login.py
@@ -14,20 +14,18 @@ class Test_TCLogin(unittest.TestCase):
         inst.driver.get('https://opensource-demo.orangehrmlive.com/')
         inst.ObjPageLogin = PageLogin(inst.driver)
 
+    def test_A_ValidateUsernameCannotBeEmptyErrorMessage(self):
+        ErrorMessage = self.ObjPageLogin.UsernameCannotBeEmptyLogin()
+        time.sleep(5)
+        self.assertEqual(ErrorMessage, "Username cannot be empty",'Incorrect Error Message: Username cannot be empty')
 
-    def test_ValidateLoginWithInValidUser(self):   
+    def test_B_ValidateLoginWithInValidUser(self):   
         ErrorMessage = self.ObjPageLogin.InvalidCredentialsLogin("Admin1","admin1231")
         time.sleep(5)
         self.assertEqual(ErrorMessage, "Invalid credentials",'Incorrect Error Message: Invalid credentials')
 
-
-    def test_ValidateLoginWithValidUser(self):
+    def test_C_ValidateLoginWithValidUser(self):
         self.ObjPageLogin.Login("Admin","admin123")
-
-    #def test_ValidateUsernameCannotBeEmptyErrorMessage(self):
-        #ErrorMessage = self.ObjPageLogin.UsernameCannotBeEmptyLogin()
-        #self.assertEqual(ErrorMessage, "Username cannot be empty",'Incorrect Error Message: Username cannot be empty')
-
 
     @classmethod
     def tearDownClass(inst):
